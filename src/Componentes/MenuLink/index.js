d65db7f09323bd8d02c32ecd3e988f6bc07e3be4
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2024-04-16 17:54:42 -0300

    nova estruturação de codigo com o Navlink

--- src/Componentes/MenuLink/index.js
+++ src/Componentes/MenuLink/index.js
@@ -1,13 +1,27 @@
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './MenuLink.module.css';
 
 export default function MenuLink ({children, to}) {
-  const localizacao = useLocation();
+  
 
 
   return (
-    <Link className={`${styles.link} ${localizacao.pathname === to ? styles.linkDestacado : ""}`} to={to}>
-          {children}
-        </Link>
-  )
+    <NavLink
+    //Como o nome indica, a propriedade isActive (do inglês "está ativo") é true se o link estiver ativo e false se não estiver
+  className={({ isActive }) => ` 
+            ${styles.link}
+            ${isActive ? styles.linkDestacado : ""}
+        `}
+        to={to}
+        end
+    // para evitar que os dois links fique com o underline, coloque o end no final 
+    >
+        {children}
+    </NavLink>
+)
+   
 }
+
+// essa seria uma outra opção caso nao quisesse usar o NavLink
+// const localizacao = useLocation();
+//<Link className={`${styles.link} ${localizacao.pathname === to ? styles.linkDestacado : ""}`} to={to}> {children}</Link>)}
