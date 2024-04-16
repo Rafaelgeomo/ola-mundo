6b84f37fd821d4aa99f0d6e2c4491b52b137a643
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2024-04-16 16:45:29 -0300

    instalação do react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./Componentes/Inicio";
+import SobreMim from "./Componentes/SobreMim";
+
 function App() {
   return (
-    <h1> Olá Mundo!</h1>
-  );
-}
+    <BrowserRouter>    
+      <Routes> {/* O componente Routes é responsável por alternar entre diferentes rotas da nossa aplicação e recebe componentes Route como conteúdo */}
+        <Route path="/" element={<Inicio/>}/>
+        <Route path="/sobremim" element={<SobreMim/>}/>
+        <Route path="*" element={<div>Página não encontrada</div>}/>
+      </Routes>
+    </BrowserRouter>
+
+  )
+};
 
 export default App;
+
