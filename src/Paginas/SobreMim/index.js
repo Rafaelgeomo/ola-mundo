b0acfbf581f8b95f3d71371ca8650bd86613cc84
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2024-04-23 15:45:16 -0300

    pagina sobre mim concluida

--- src/Paginas/SobreMim/index.js
+++ src/Paginas/SobreMim/index.js
@@ -1,5 +1,40 @@
+import styles from './SobreMim.module.css';
+import PostModelo from "Componentes/PostModelo";
+import fotoCapa from 'assets/sobre_mim_capa.png';
+import fotoSobreMim from 'assets/sobre_mim_foto.png';
+
+
+
 export default function SobreMim(){
   return (
-    <h1> Sobre mim... </h1>
+    <PostModelo  
+      fotoCapa={fotoCapa}
+      titulo="Sobre mim"
+    >
+
+     <h2 className={styles.subtitulo}>
+        Olá, eu sou Rafael Jeronymo!
+     </h2>
+
+     <img 
+        src={fotoSobreMim}
+        alt='Foto do Rafael '
+        className={styles.fotoSobreMim}
+      />
+      <p className={styles.paragrafo}>Aprendiz de Código e Aventureiro Digital</p>
+      <p className={styles.paragrafo}>
+      E aí, pessoal! Me chamo Rafael Jeronymo e sou um cara que trabalhou por 17 anos na área de publicidade em outdoors. Agora tô mudando de ares e me aventurando no mundo da programação.
+      </p>
+      <p className={styles.paragrafo}>
+      Tô no 4º semestre da faculdade de desenvolvimento de software e, apesar de ter me formado em Logística, tô focado em seguir nessa nova área que tanto me encanta.
+      </p>
+      <p className={styles.paragrafo}>
+      Tenho feito vários cursos legais, como lógica de programação, HTML5, CSS3 e JavaScript. E agora tô começando a me aprofundar em React, onde tô aprendendo um montão de coisas novas.
+      </p>
+      <p className={styles.paragrafo}>
+      Tô super animado com as possibilidades que a programação oferece e tô sempre em busca de desafios e oportunidades para continuar aprendendo e crescendo nessa área.
+      </p>
+     
+    </PostModelo>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Inicio from "./Paginas/Inicio";
 import SobreMim from "./Paginas/SobreMim";
 import Menu from "./Componentes/Menu";
 import Rodape from "Componentes/Rodape";
+import PaginaPadrao from "Componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
@@ -10,8 +11,12 @@ function AppRoutes() {
       <Menu/>
 
       <Routes> {/* O componente Routes é responsável por alternar entre diferentes rotas da nossa aplicação e recebe componentes Route como conteúdo */}
-        <Route path="/" element={<Inicio/>}/>
-        <Route path="/sobremim" element={<SobreMim/>}/>
+        <Route path="/" element={<PaginaPadrao/>}>
+          <Route index element={<Inicio/>}/>
+          <Route path="sobremim" element={<SobreMim/>}/>
+
+        </Route>       
+        
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
 
@@ -22,4 +27,20 @@ function AppRoutes() {
 };
 
 export default AppRoutes;
+
+
+ 
+        // Na rota "/", a estruta a ser renderizada é:
+
+        // <PaginaPadrao>
+        //   <Inicio/>    
+        // </PaginaPadrao>
+
+        // Na rota "/sobremim", a estruta a ser renderizada é:
+
+        // <PaginaPadrao>
+        //   <SobreMim/>    
+        // </PaginaPadrao>
+        
+        
 
